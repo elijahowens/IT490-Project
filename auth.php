ac045a4759fb5b0ace03cfdde74260070cfcfa88
marlonegoavil <mae7@njit.edu>
2023-10-17 10:38:51 -0400

    transferring file code test

--- auth.php
+++ auth.php
@@ -1,1 +1,35 @@
+<?php
+
+$conn = new mysqli("127.0.0.1", "marlonegoavil", 'Paul.5600', 'users');
+
+if($conn->connect_error)
+{
+        die("Connection failed: " . $conn->connect_error);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+
+        $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sss", $username, $email, $password);
+
+        if($stmt->execute())
+        {
+                echo "Registration successful!";
+
+        }
+        else
+        {
+                echo "Error: " . $conn->error;
+        }
+
+        $stmt->close();
+}
+
+$conn->close();
+?>
 
