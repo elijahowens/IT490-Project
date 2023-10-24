0725887f9605ff2af3843cab1b4c3441ee37db04
elijahowens <56016850+elijahowens@users.noreply.github.com>
2023-10-24 19:14:04 -0400

    Update auth.php

--- auth.php
+++ auth.php
@@ -16,7 +16,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         $sql = "INSERT INTO user_info (username, email, password) VALUES (?, ?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("sss", $username, $email, $password);
-        error_log(print_r($row,true));
 
         if($stmt->execute())
         {
