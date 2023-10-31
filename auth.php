c1b4c15494635f28ad35d978ea13182d67c5c331
elijahowens <56016850+elijahowens@users.noreply.github.com>
2023-10-31 03:07:51 -0400

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,40 +1,34 @@
 <?php
 $hostname = "localhost"; // Hostname
-$username = "eco9"; // MySQL username
-$password = "IT490Rocks!"; // MySQL password
+$db_username = "eco9"; // MySQL username
+$db_password = "IT490Rocks!"; // MySQL password
 $database = "users"; // Database name
-$conn = new mysqli($hostname, $username, $password, $database);
 
-if($conn->connect_error)
-{
-        die("Connection failed: " . $conn->connect_error);
+$conn = new mysqli($hostname, $db_username, $db_password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user_input_username = $_POST["username"];
+    $user_input_email = $_POST["email"];
+    $user_input_password = password_hash($_POST["password"], PASSWORD_BCRYPT);
 
-        $sql = "INSERT INTO user_info (username, email, password) VALUES (?, ?, ?)";
-        // Print the SQL query for debugging
-        echo "SQL Query: " . $sql . "<br>";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sss", $username, $email, $password);
+    $sql = "INSERT INTO user_info (username, email, password) VALUES (?, ?, ?)";
+    
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $user_input_username, $user_input_email, $user_input_password);
 
-        if($stmt->execute())
-        {
-                echo "Registration successful!";
+    if ($stmt->execute()) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
 
-        }
-        else
-        {
-                echo "Error: " . $conn->error;
-        }
-
-        $stmt->close();
+    $stmt->close();
 }
 
 $conn->close();
 ?>
-
