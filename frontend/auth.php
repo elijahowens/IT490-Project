67bb450c142748409c1e07330efb976c50a10fa3
elijahowens <56016850+elijahowens@users.noreply.github.com>
2023-11-06 17:31:02 -0500

    Update auth.php
    included new dbconnect file

--- frontend/auth.php
+++ frontend/auth.php
@@ -2,20 +2,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-
-$hostname = "localhost"; // Hostname
-$db_username = "eco9"; // MySQL username
-$db_password = "IT490Rocks!"; // MySQL password
-$database = "users"; // Database name
-
-$conn = new mysqli($hostname, $db_username, $db_password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else {
-    echo "Connected ";
-}
+include 'dbconnect.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user_input_username = $_POST["username"];
