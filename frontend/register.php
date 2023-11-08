dc1f8ad994f2bdafc73a2d6831e3b1c8eb898000
elijahowens <56016850+elijahowens@users.noreply.github.com>
2023-11-07 23:56:01 -0500

    Update register.php

--- frontend/register.php
+++ frontend/register.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'dbconnect.php';
+require_once('dbconnect.php');
 include 'RabbitMQFunctions.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
